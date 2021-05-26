c952eb786560b63c924c0f06f3d14758f5806a8b
Tom Reis <tom@ckdtech.co>
2021-05-25 21:57:44 -0600

    Update main page styles

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
-import Head from 'next/head'
-import AudioPlayer from '../components/AudioPlayer';
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import AudioPlayer from "../components/AudioPlayer";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const cloudfrontUrl = "https://dacoj7br0jajz.cloudfront.net"
+  const cloudfrontUrl = "https://dacoj7br0jajz.cloudfront.net";
   const tracks = [
-    { title: 'Holding Pattern', audioSrc: `${cloudfrontUrl}/holding_pattern.wav` },
-    { title: 'Moon and I', audioSrc: `${cloudfrontUrl}/moon.wav` },
-    { title: 'Wallflower', audioSrc: `${cloudfrontUrl}/wallflower.wav` }
-  ]
+    {
+      title: "Holding Pattern",
+      audioSrc: `${cloudfrontUrl}/holding_pattern.wav`,
+    },
+    { title: "Moon and I", audioSrc: `${cloudfrontUrl}/moon.wav` },
+    { title: "Wallflower", audioSrc: `${cloudfrontUrl}/wallflower.wav` },
+  ];
 
   return (
     <div className={styles.container}>
@@ -16,20 +19,25 @@ export default function Home() {
         <title>Lost in the Mail</title>
         <meta name="description" content="Website for Lost in the Mail band" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Courier+Prime&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          We are Lost in the Mail
+          We are <br /> Lost in the Mail
         </h1>
 
         <section className={styles.playerContainer}>
           {/* AudioPlayer uses the HTMLAudioElement interface,
               which is only available in Browser
               (https://developer.mozilla.org/en-US/docs/Web/API/HTMLAudioElement) */}
-          { process.browser && <AudioPlayer tracks={tracks} /> }
+          {process.browser && <AudioPlayer tracks={tracks} />}
         </section>
       </main>
     </div>
-  )
+  );
 }
