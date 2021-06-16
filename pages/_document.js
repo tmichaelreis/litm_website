c4859b02987ed7a69cf6918223c62dca99b5f733
Tom Reis <tom@ckdtech.co>
2021-06-16 09:19:11 -0600

    Remove hardcoded gtag from document file

--- pages/_document.js
+++ pages/_document.js
@@ -8,16 +8,19 @@ export default class MyDocument extends Document {
           {/* Google Analytics */}
           <script
             async
-            src="https://www.googletagmanager.com/gtag/js?id=G-F74W0X0XY9"
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
           ></script>
           <script
             dangerouslySetHtml={{
               __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
 
-            gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');`,
+                gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
             }}
           />
         </Head>
