13157c863f292b83c28b303cffa6a55eff524261
Tom Reis <tom@ckdtech.co>
2021-06-16 23:06:12 -0600

    Fix analytics

--- pages/_document.js
+++ pages/_document.js
@@ -10,19 +10,7 @@ export default class MyDocument extends Document {
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
           ></script>
-          <script
-            dangerouslySetHtml={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-
-                gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}', {
-                  page_path: window.location.pathname,
-                });
-              `,
-            }}
-          />
+          <script src="/analytics.js" async></script>
         </Head>
         <body>
           <Main />
---
+++ public/analytics.js
@@ -0,0 +1,9 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+gtag("config", "G-F74W0X0XY9", {
+  page_path: window.location.pathname,
+});
